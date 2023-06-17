01d1f645dbbac232179ed2f09e6ca491597c36ee
rubens23 <rubens_assis@outlook.com.br>
2023-06-17 10:54:51 -0300

    adicionando imagem da arquitetura

--- app/src/main/java/com/example/appdenunciacliente/framework/viewModels/LoginFragmentViewModel.kt
+++ app/src/main/java/com/example/appdenunciacliente/framework/viewModels/LoginFragmentViewModel.kt
@@ -1,25 +1,12 @@
 package com.example.appdenunciacliente.framework.viewModels
 
-import android.app.Activity
-import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
+
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asFlow
-import androidx.lifecycle.liveData
 import androidx.lifecycle.viewModelScope
 import com.example.appdenunciacliente.framework.managers.LoginManager
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.drop
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
